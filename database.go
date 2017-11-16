b8fc9f8e634fd0efae1d6a73c43d01afe577859b
astrothing <andres.rodriguezhn@gmail.com>
2017-11-16 14:04:29 -0600

    Added return value to ExecuteCommand

--- database.go
+++ database.go
@@ -105,14 +105,19 @@ func (addr Address) offset() int64 {
 	return int64(metadataBlockSize + blockSize*(addr-1))
 }
 
-func (db *Database) ExecuteCommand(cmd interface{}) error {
+/*
+ExecuteCommand runs the command and returns a result object as interface{} if there is any.
+Returns an error if there was one.
+*/
+func (db *Database) ExecuteCommand(cmd interface{}) (interface{}, error) {
 	switch cmd := cmd.(type) {
 	case common.CreateTableCommand:
-		_, err := db.NewTable(cmd.TableName(), cmd.TableColumnDefiners())
-		return err
+		return db.NewTable(cmd.TableName(), cmd.TableColumnDefiners())
 	case common.InsertCommand:
-		return db.Insert(cmd.TableName(), cmd.Values())
+		return nil, db.Insert(cmd.TableName(), cmd.Values())
+	case common.SelectTableCommand:
+		return db.ReadTable(cmd.SourceTable())
 	}
 
-	return errors.New("Unrecognized command")
+	return nil, errors.New("Unrecognized command")
 }
