aa5ed893b9cb6243c44a7c4d8b1ed297bbac4c3c
robertohf <robertohf15@gmail.com>
2017-11-15 22:31:25 -0600

    Insert Function

--- table.go
+++ table.go
@@ -100,3 +100,52 @@ func (db Database) ReadTable(tableName string) (*ResultSet, error) {
 
 	return &ResultSet{Keys: keys, Rows: rows}, nil
 }
+
+func (db Database) Insert(tableName string, values map[string]interface{}) error {
+	tableEntry, err := db.findTableEntry(tableName)
+	if err != nil {
+		return err
+	}
+	tableHeaderBlock, err := db.readHeaderBlock(tableEntry.HeaderBlock)
+	if err != nil {
+		return err
+	}
+
+	block, err := db.readBlock(tableEntry.HeaderBlock)
+	if err != nil {
+		return err
+	}
+
+	recordSize, _ := tableHeaderBlock.recordReaders()
+	tableColumns := tableHeaderBlock.TableColumns()
+	rows := []Row{}
+
+	for recordBlockAddr := tableHeaderBlock.FirstRecordBlock; recordBlockAddr != nullBlockAddr; {
+		recordBlock, err := db.readRecordBlock(tableHeaderBlock.FirstRecordBlock)
+		if err != nil {
+			return err
+		}
+
+		for _, record := range recordBlock.Data.split(recordSize) {
+			if record.isFree() {
+				continue
+			}
+
+			row := Row{}
+			for _, tableColumn := range tableColumns {
+				columnName := tableColumn.ColumnName()
+				if columnName == values[columnName].(string) {
+					row[columnName] = values[columnName].(string)
+				}
+			}
+			rows = append(rows, row)
+		}
+
+		err = db.writeBlock(recordBlockAddr, block)
+		if err != nil {
+			return err
+		}
+		recordBlockAddr = recordBlock.NextRecordBlock
+	}
+	return nil
+}
