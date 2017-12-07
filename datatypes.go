cadc9e99e0b083b97965d8506f9c3a814d99cdd5
astrothing <andres.rodriguezhn@gmail.com>
2017-12-07 17:18:44 -0600

    Added data types

---
+++ datatypes.go
@@ -0,0 +1,45 @@
+package data
+
+type dbTypeID uint8
+
+const (
+	dbIntegerTypeID dbTypeID = iota
+	dbFloatTypeID
+	dbDateTimeTypeID
+	dbBooleanTypeID
+	dbCharTypeID
+)
+
+type dbDataType interface {
+	dbTypeID() dbTypeID
+}
+
+type dbInteger int64
+
+func (dt dbInteger) dbTypeID() dbTypeID {
+	return dbIntegerTypeID
+}
+
+type dbFloat float64
+
+func (dt dbFloat) dbTypeID() dbTypeID {
+	return dbFloatTypeID
+}
+
+type dbDateTime int64
+
+func (dt dbDateTime) dbTypeID() dbTypeID {
+	return dbDateTimeTypeID
+}
+
+type dbBoolean bool
+
+func (dt dbBoolean) dbTypeID() dbTypeID {
+	return dbBooleanTypeID
+}
+
+type dbChar []byte
+
+func (dt dbChar) dbTypeID() dbTypeID {
+	return dbCharTypeID
+}
