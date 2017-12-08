65a17735f8fac3cbb9011f7a9566e2b7d06725a9
astrothing <andres.rodriguezhn@gmail.com>
2017-12-07 19:50:14 -0600

    Added add and delete constraint functions to dbColumn struct

--- dbcolumn.go
+++ dbcolumn.go
@@ -1,5 +1,7 @@
 package data
 
+import "fmt"
+
 type dbConstraintType uint8
 
 const (
@@ -10,11 +12,20 @@ const (
 	dbDefaultValueConstraint
 )
 
+var dbConstraintTypeNames = map[dbConstraintType]string{
+	dbPrimaryKeyConstraint:    "PRIMARY_KEY",
+	dbForeignKeyConstraint:    "FOREIGN_KEY",
+	dbAutoincrementConstraint: "AUTOINCREMENT",
+	dbNotNullConstraint:       "NOT_NULL",
+	dbDefaultValueConstraint:  "DEFAULT_VALUE",
+}
+
 type dbColumn struct {
 	dbTableID                  dbInteger
 	dbColumnID                 dbInteger
 	dbTypeID                   dbTypeID
 	dbColumnName               dbChar
+	dbAutoincrementCounter     dbInteger
 	dbDefaultValueConstraintID dbInteger
 	dbConstraints              dbConstraintType
 }
@@ -25,4 +36,31 @@ func (dc dbColumn) name() string {
 
 func (dc dbColumn) hasConstraint(constraint dbConstraintType) bool {
 	return (dc.dbConstraints & constraint) != 0
+}
+
+func (dc *dbColumn) addConstraint(constraint dbConstraintType) error {
+	if dc.hasConstraint(constraint) {
+		return fmt.Errorf("Duplicate `%s' constraint on column `%s'", dbConstraintTypeNames[constraint], dc.name())
+	}
+
+	dc.dbConstraints |= constraint
+	return nil
+}
+
+func (dc *dbColumn) deleteConstraint(constraint dbConstraintType) error {
+	if !dc.hasConstraint(constraint) {
+		return fmt.Errorf("Column `%s' does not have `%s' constraint", dc.name(), dbConstraintTypeNames[constraint])
+	}
+
+	dc.dbConstraints &^= constraint
+	return nil
+}
+
+func (dc *dbColumn) increment() error {
+	if !dc.hasConstraint(dbAutoincrementConstraint) {
+		return fmt.Errorf("Column `%s' does not have autoincrement constraint", dc.name())
+	}
+
+	dc.dbAutoincrementCounter++
+	return nil
 }
